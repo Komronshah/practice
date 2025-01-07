cd2b95d5d1f1b23f1099d138ebc5a436c4c953c0
Rahimov Komron <komronshahrahimov2005@gmail.com>
2025-01-07 18:31:12 +0500

    first c

---
+++ Test/Program.cs
@@ -0,0 +1,16 @@
+﻿
+int[] numbers = { 1, 2, 3 };
+Console.WriteLine("Array Elements: ");
+
+foreach (int num in numbers)
+{
+    Console.WriteLine(Math.Pow(num,2));
+}
+
+Console.WriteLine("----------------------------------------");
+
+string[] names = { "Muhammad", "Nurullo", "Test" };
+foreach (var name in names)
+{
+    Console.WriteLine(name);
+}
